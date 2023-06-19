e357e22cbf084089e2f37f3800c950d8d4741515
SavaStyle <sava.90@inbox.ru>
2023-06-19 19:05:07 +0300

    City

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,40 @@
 package org.example;
 
+import org.example.model.City;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String fileName = "resources/Задача ВС Java Сбер.csv";
+        Path path = Paths.get(fileName);
+        Scanner scanner = new Scanner(path);
+        scanner.useDelimiter("\r?\n");
 
+        while (scanner.hasNext()) {
+            City city = parseCSVLine(scanner.next());
+            System.out.println(city + "\n");
+        }
+        scanner.close();
+    }
+
+    private static City parseCSVLine(String next) {
+        String[] lines = next.split(";");
+
+        City nextCity = new City();
+        nextCity.setName(lines[1]);
+        nextCity.setRegion(lines[2]);
+        nextCity.setDistrict(lines[3]);
+        nextCity.setPopulation(Long.parseLong(lines[4]));
+        if (lines.length == 6) {
+            nextCity.setFoundation(lines[5]);
+        } else {
+            nextCity.setFoundation(" ");
+        }
+        return nextCity;
     }
 }
---
+++ src/main/java/org/example/model/City.java
@@ -0,0 +1,30 @@
+package org.example.model;
+
+import lombok.AccessLevel;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class City {
+    String name;
+    String region;
+    String district;
+    Long population;
+    String foundation;
+
+    public City() {
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation='" + foundation + '\'' +
+                '}';
+    }
+}
